f82541367e89104b12577e39667f076bce584146
Huzaifa Ali Khan <azmathuzaifa@gmail.com>
2025-08-09 19:14:48 +0500

    fix: add return statement in axios call

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+const port = 3000;
+const API_URL = "https://www.thecocktaildb.com/api/json/v1/1";
+
 // Homepage with search form
 app.get('/', (req, res) => {
-  res.render('index', { drinks: null, searchTerm: null });
+  return res.render('index', { drinks: null, searchTerm: null });
 });
 
 // Search results
@@ -19,25 +22,25 @@ app.get('/search', async (req, res) => {
   }
 
   try {
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`;
-    const response = await axios.get(url);
-    res.render('index', { drinks: response.data.drinks, searchTerm });
+    const response = await axios.get(API_URL + `/search.php?s=${searchTerm}`);
+    return res.render('index', { drinks: response.data.drinks, searchTerm });
   } catch (error) {
     console.error(error.message);
-    res.status(500).send('Error fetching drinks.');
+    return res.status(500).send('Error fetching drinks.');
   }
 });
 
 // Drink details
 app.get('/cocktail/:id', async (req, res) => {
   try {
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${req.params.id}`;
-    const response = await axios.get(url);
-    res.render('cocktail', { drink: response.data.drinks[0] });
+    const response = await axios.get(API_URL + `/lookup.php?i=${req.params.id}`);
+    return res.render('cocktail', { drink: response.data.drinks[0] });
   } catch (error) {
     console.error(error.message);
-    res.status(500).send('Error fetching drink details.');
+    return res.status(500).send('Error fetching drink details.');
   }
 });
 
-app.listen(3000, () => console.log('Server running at http://localhost:3000'));
+app.listen(3000, () => 
+  console.log(`Server running at http://localhost:${port}`)
+);
